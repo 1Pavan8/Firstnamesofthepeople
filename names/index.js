76237128da03f08c09f0daaba2028a1e06045f8a
Pavan <mallelapkr18@gmail.com>
2023-08-24 14:25:37 +0000

    exported names and function and returned array of names

---
+++ names/index.js
@@ -0,0 +1,7 @@
+const lis = require("../country/state/city/index");
+const fun = require("../utilities/utils/index");
+
+function getPeopleInCity(lis) {
+  return fun(lis);
+}
+module.exports = getPeopleInCity;
